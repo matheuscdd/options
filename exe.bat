38b32b613dc9abe813e132ef0cb231cf3b515037
Claus <103610806+matheuscdd@users.noreply.github.com>
2023-12-19 11:15:19 -0300

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,4 +1,4 @@
 start msedge "https://code.visualstudio.com/docs/?dv=win64user"
-start msedge "https://code.visualstudio.com/insiders/?dv=win64user"
+start msedge "https://code.visualstudio.com/docs/?dv=win&build=insiders"
 start msedge "http://kmate.me/data/uploads/download/KindleMate138_FullInstaller.exe"
 start msedge "https://www.kaspersky.com.br/downloads/total-security"
