ae26ecdce45e8af51e0aa59724293ea56cbf20b9
Claus <103610806+matheuscdd@users.noreply.github.com>
2024-02-01 14:22:33 -0300

    Update custom_bash.sh

--- custom_bash.sh
+++ custom_bash.sh
@@ -1,9 +1,12 @@
+yor='yor@192.168.1.150'vmdow
 alias github="git config --get remote.origin.url | sed 's/git@github.com:/https:\/\/github.com\//' | sed 's/.git$//'"
 alias copypwd="pwd | xclip -selection clipboard"
 alias python="python3.10"
 alias vactive="source .venv/bin/activate"
 alias bat=batcat
-alias vm='VBoxManage startvm "ubuntu_server" --type headless && sleep 60 && ssh yor@192.168.1.150'
+alias vmup="VBoxManage startvm 'ubuntu_server' --type headless && Loader && ssh $yor"
+alias vmdown='echo "VM "ubuntu_server" is down" && VBoxManage controlvm "ubuntu_server" acpipowerbutton'
+alias vm="ssh yor@192.168.1.150"
 alias update_firefox='wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O Firefox-dev.tar.bz2 && sudo tar xjf  Firefox-dev.tar.bz2 -C /opt/ && rm -r Firefox-dev.tar.bz2'
 alias ..="cd .."
 alias selsh="docker exec -it portal_stream_selenium_test-web-1 python manage.py"
@@ -20,5 +23,13 @@ sf() {
     local new="sf_$curr-$_date"
     git branch "$new"
     echo "$new"
+}
+Loader() {
+  for i in $(seq 0 0.1 600); do
+    echo -ne "\rVM \"ubuntu_server\" is loading...$i%" 
+    sleep 0.1
+  done
+  tput cnorm 
+  echo ''
 }
 export BAT_THEME="GitHub"
