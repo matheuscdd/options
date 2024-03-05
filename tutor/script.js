499c13e58aa212269e6e3208c59608e024e0b6b0
matheuscdd <matheuscdd@hotmail.com>
2024-03-05 20:03:32 -0300

    feat: add sound

--- tutor/script.js
+++ tutor/script.js
@@ -1,2 +1,16 @@
+const audio = document.createElement('audio');
 const source = document.createElement('source');
-source.src = 
+source.src = 'https://github.com/matheuscdd/options/raw/main/tutor/aleluia.mp3';
+source.type = 'audio/mpeg'
+audio.style.display = 'none';
+audio.appendChild(source);
+
+function verifyTax() {
+  const container = document.querySelector('.dynamic-rate-box');
+  if (!container) return;
+  if (container.innerText.includes('Redação')) {
+    audio.play();
+  }
+  setTimeout(() => verifyTax(), 30 * 1000)
+}
+verifyTax();
