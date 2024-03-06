7942631db7e858f1b241dc3c5ffb429574e59b37
matheuscdd <matheuscdd@hotmail.com>
2024-03-05 22:28:46 -0300

    audio

--- tutor/script.js
+++ tutor/script.js
@@ -1,15 +1,25 @@
-const audio = document.createElement('audio');
-const source = document.createElement('source');
-source.src = 'https://github.com/matheuscdd/options/raw/main/tutor/aleluia.mp3';
-source.type = 'audio/mpeg'
-audio.style.display = 'none';
-audio.appendChild(source);
+function createAudio(src) {
+  const audio = document.createElement('audio');
+  const source = document.createElement('source');
+  source.src = src;
+  source.type = 'audio/mpeg'
+  audio.style.display = 'none';
+  audio.appendChild(source);
+  return audio;
+}
 
+const success = createAudio('https://github.com/matheuscdd/options/raw/main/tutor/aleluia.mp3')
+const fail = createAudio('');
+
+let tax = false;
 function verifyTax() {
+  const old = tax;
   const container = document.querySelector('.dynamic-rate-box');
   if (!container) return;
-  if (container.innerText.includes('Redação')) audio.play();
-  setTimeout(() => verifyTax(), 5000 * 1000)
+
+  tax = container.innerText.includes('Redação');
+  if (tax !== old) tax ? success.play() : fail.play();
+  setTimeout(() => verifyTax(), 1000 * 1000)
 }
 setTimeout(() => verifyTax(), 20 * 1000);
 console.log('executou')
