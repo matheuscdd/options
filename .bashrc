abb6f3b0c1ac3e22af811f9cfba07bfaa25e7d2f
Claus <103610806+matheuscdd@users.noreply.github.com>
2024-05-04 19:04:57 -0300

    linux

--- .bashrc
+++ .bashrc
@@ -10,18 +10,8 @@ alias vm="ssh $yor"
 alias vma="echo a | vm sudo -S"
 alias update_firefox='wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O Firefox-dev.tar.bz2 && sudo tar xjf  Firefox-dev.tar.bz2 -C /opt/ && rm -r Firefox-dev.tar.bz2'
 alias ..="cd .."
-alias selsh="docker exec -it portal_stream_test-web-1 python manage.py"
-alias porsh="docker exec -it portal_stream-web-1 python manage.py"
-alias sel="docker compose -f docker-compose.test.yml -p portal_stream_test"
-alias por="docker compose -f docker-compose.dev.yml"
-alias selup="docker compose -f docker-compose.test.yml -p portal_stream_test up"
-alias porup="docker compose -f docker-compose.dev.yml up"
-alias selstop="docker compose -f docker-compose.test.yml -p portal_stream_test stop"
-alias porstop="docker compose -f docker-compose.dev.yml stop"
-alias seldown="docker compose -f docker-compose.test.yml -p portal_stream_test down"
-alias pordown="docker compose -f docker-compose.dev.yml down"
 alias copy="xclip -selection clipboard"
-
+alias sudo="sudo "
 mkcd() {
   mkdir -p "$1" && cd "$1"
 }
@@ -41,3 +31,4 @@ Loader() {
   echo ''
 }
 export BAT_THEME="GitHub"
+xmodmap ~/.Xmodmap
---
+++ pipeline.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+USER=$(ls /home)
+sudo apt update && sudo apt upgrade -y
+sudo apt remove -y xfce4-terminal
+sudo snap refresh
+sudo flatpak update -y
+sudo apt install -y jq bat xclip flameshot gpick wmctrl xdotool xbindkeys playerctl fonts-cascadia-code
+
+sudo flatpak install --noninteractive flathub io.dbeaver.DBeaverCommunity rest.insomnia.Insomnia org.chromium.Chromium com.obsproject.Studio us.zoom.Zoom com.mongodb.Compass com.github.hluk.copyq com.vixalien.sticky com.brave.Browser
+
+sudo snap install code --classic 
+sudo snap install code-insiders --classic 
+sudo snap install node --classic 
+sudo snap install shotcut --classic 
+sudo snap install gh --classic 
+
+# ulaucher
+sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher -y && sudo apt update && sudo apt install ulauncher -y
+
+# python
+sudo apt install -y software-properties-common 
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.10 python3.10-venv 
+python3.10 --version
+
+# Firefox Dev
+wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O Firefox-dev.tar.bz2
+sudo tar xjf Firefox-dev.tar.bz2 -C /opt/
+rm -r Firefox-dev.tar.bz2
+sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox-dev
+sudo chown claus /opt/firefox
+sudo cp /opt/firefox/browser/chrome/icons/default/default32.png /opt/firefox/browser/chrome/icons/default/application-x-executable.png
+sudo cp /opt/firefox-developer/browser/chrome/icons/default/custom/application-x-executable.png /usr/share/icons/Yaru/24x24@2x/mimetypes/
+sudo cp /opt/firefox-developer/browser/chrome/icons/default/custom/application-x-executable.png /usr/share/icons/Yaru/24x24/mimetypes/
+
+# chrome-dev
+https://www.google.com/chrome/dev/next-steps.html?installdataindex=empty&statcb=0&defaultbrowser=0
+
+# Docker
+sudo apt update
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+sudo usermod -aG docker claus
+
+# aws
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws configure
+
+
+# Atalhos de teclado
+sudo wget -P "/home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml" 'https://raw.githubusercontent.com/matheuscdd/options/main/xfce4-keyboard-shortcuts.xml'
+
+# Ícone menu iniciar
+wget 'https://raw.githubusercontent.com/matheuscdd/options/main/assets/xfce4-zorinmenulite-symbolic.svg'
+sudo cp xfce4-zorinmenulite-symbolic.svg /usr/share/icons/hicolor/scalable/apps
+sudo cp xfce4-zorinmenulite-symbolic.svg /usr/share/icons/hicolor/symbolic/apps
+
+# Ícone notas
+wget 'https://raw.githubusercontent.com/matheuscdd/options/main/assets/com.vixalien.sticky.svg'
+sudo cp com.vixalien.sticky.svg /var/lib/flatpak/exports/share/icons/hicolor/scalable/apps
+gtk-update-icon-cache -ft "/home/$USER/.local/share/icons/hicolor"
+
+# Ajustar atalho mongo e firefox
+str=$(cat <<EOF
+#!/bin/bash
+
+rawWindow=$(xdotool getactivewindow getwindowname)
+IFS="-"
+read -ra handleWindow <<< "$rawWindow"
+xbindkeys
+if [[ "${handleWindow[0]}" == "MongoDB Compass " ]]; then
+	sleep 0.1 && xdotool key 0xff0d
+else
+	killall xbindkeys
+	sleep 0.1
+	xdotool key --clearmodifiers "Control+Return"
+fi
+IFS=""
+sleep 0.1
+xbindkeys
+
+EOF
+)
+
+sudo echo "$str" > /usr/local/bin/mongoenter.sh && sudo chmod +x /usr/local/bin/mongoenter.sh
+
+str=$(cat <<EOF
+#!/bin/bash
+
+has=$(xdotool getactivewindow getwindowname | grep -q "Firefox" && echo "1" || "2")
+
+xbindkeys
+if [[ -n "$has" ]]; then
+	sleep 0.1 && xdotool key --clearmodifiers "Control+Shift+P"
+else
+	killall xbindkeys
+	sleep 0.1
+	xdotool key --clearmodifiers "Control+Shift+N"
+fi
+sleep 0.1
+xbindkeys
+
+EOF
+)
+
+sudo echo "$str" > /usr/local/bin/firo.sh
+sudo chmod +x /usr/local/bin/firo.sh
+
+str=$(cat <<EOF
+"/usr/local/bin/mongoenter.sh"
+  Control + Return
+  
+"/usr/local/bin/mongoenter.sh"
+  Control + KP_Enter
+  
+"/usr/local/bin/firo.sh"
+  Control + Shift + N
+EOF
+)
+
+sudo echo "$str" > "/home/$USER/.xbindkeysrc"
+
+# Ajustar atalhos de teclado
+str=$(cat <<EOF
+keycode 18 = parenleft
+keycode 19 = parenright
+keycode 134 = braceleft
+keycode 135 = braceright
+EOF
+)
+
+sudo echo "$str" > "/home/$USER/.Xmodmap"
+xmodmap "/home/$USER/.Xmodmap"
+
+# Sticky
+str=$(cat <<EOF
+#!/bin/bash
+
+sleep 30 && 
+EOF
+)
+
+sudo echo "$str" > /usr/local/bin/sticky.sh
+sudo chmod 777 /usr/local/bin/sticky.sh
+
+str=$(cat <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Version=0.9.4
+Type=Application
+Name=Sticky
+Comment=
+Exec=/usr/local/bin/sticky.sh
+OnlyShowIn=XFCE;
+RunHook=0
+StartupNotify=false
+Terminal=false
+Hidden=false
+EOF
+)
+sudo echo "$str" > "/home/$USER/.config/autostart/Sticky.desktop"
+sudo chmod +x "/home/$USER/.config/autostart/Sticky.desktop"
+
+# numlock
+str=$(cat <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Version=0.9.4
+Type=Application
+Name=numlock
+Comment=
+Exec=sleep 20 && numlockx on
+OnlyShowIn=XFCE;
+RunHook=0
+StartupNotify=false
+Terminal=false
+Hidden=false
+EOF
+)
+sudo echo "$str" > "/home/$USER/.config/autostart/numlock.desktop"
+sudo chmod +x "/home/$USER/.config/autostart/numlock.desktop"
+
+# Atualizar o bashrc
+# TODO preciso ajustar para só dar o append no atual
+sudo wget -P "/home/$USER/" 'https://raw.githubusercontent.com/matheuscdd/options/main/.bashrc'
+
+# Inserir ícones
+ICONS="/home/$USER/Desktop"
+wget -P "$ICONS" 'https://raw.githubusercontent.com/matheuscdd/options/main/icons_l/Firefox%20Developer%20Edition.desktop'
+file="$ICONS/Firefox Developer Edition.desktop"
+sudo chmod +x "$file" 
+sudo mv "$file" '/usr/share/applications/'
+wget -P "$ICONS" 'https://raw.githubusercontent.com/matheuscdd/options/main/icons_l/com.mongodb.Compass.desktop'
+wget -P "$ICONS" 'https://raw.githubusercontent.com/matheuscdd/options/main/icons_l/com.slack.Slack.desktop'
+wget -P "$ICONS" 'https://raw.githubusercontent.com/matheuscdd/options/main/icons_l/google-chrome-unstable.desktop'
+wget -P "$ICONS" 'https://raw.githubusercontent.com/matheuscdd/options/main/icons_l/io.dbeaver.DBeaverCommunity.desktop'
+wget -P "$ICONS" 'https://raw.githubusercontent.com/matheuscdd/options/main/icons_l/xfce4-terminal-emulator.desktop'
+
+sudo reboot
